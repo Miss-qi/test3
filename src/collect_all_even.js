8fad1206144efd3d11ab3086b5fab6faa44d97ca
Miss-qi <1370322806@qq.com>
2016-06-09 14:14:16 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,12 +1,10 @@
-
-
 function collect_same_elements(collection_a, object_b) {
  //在此处写代码
- var arr = [];
+ var arr = new Array();
  var k=0;
  for(var i=0;i<collection_a.length;i++)
  	for(var j=0;j<object_b.length;j++){
- 		if(collection_a.key[i]==object_b[j]){	
+ 		if(collection_a[i].key==object_b[j]){	
  			arr[k++]="\""+object_b[j]+"\"";
  		}
  	}
@@ -15,3 +13,4 @@ document.write("[");
 document.write(arr.join());
 document.write("]");
 module.exports = collect_same_elements;
+
