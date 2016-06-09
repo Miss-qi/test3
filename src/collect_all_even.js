24fda27202204441e2eb395dc760b0a6dfb5fd88
Miss-qi <1370322806@qq.com>
2016-06-09 13:41:20 +0800

    commit

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,19 @@
 
 function collect_same_elements(collection_a, object_b) {
  //在此处写代码
+ var arr = [];
+ var k=0;
+ for(var i=0;i<collection_a.length;i++)
+ 	for(var j=0;j<object_b.length;j++){
+ 		if(collection_a.key[i]==object_b[j]){	
+ 			arr[k++]=object_b[j];
+ 			arr[k++]=",";
+ 		}
+ 	}
 }
-
+document.write("[");
+for(i=0;i<arr.length-1;i++){
+	document.write("\""+arr[i]+"\"");
+}
+document.write("]");
 module.exports = collect_same_elements;
